4c2ea12f5ff119a02b659ea925b85209531e0b9e
Cristiano Gavião <cvgaviao@gmail.com>
2012-07-11 19:57:03 -0300

    fixed problems in the story editor and added integration to organization model

--- org.lunifera.metamodel.dsl.behavior.stories.en.tests/src/org/lunifera/metamodel/dsl/behavior/stories/en/tests/TestStoryWithMeta.xtend
+++ org.lunifera.metamodel.dsl.behavior.stories.en.tests/src/org/lunifera/metamodel/dsl/behavior/stories/en/tests/TestStoryWithMeta.xtend
@@ -1,4 +1,4 @@
-package org.lunifera.metamodel.dsl.behavior.en.tests
+package org.lunifera.metamodel.dsl.behavior.stories.en.tests
 
 import org.eclipse.xtext.junit4.XtextRunner
 import org.junit.runner.RunWith
---
+++ org.lunifera.metamodel.dsl.organization.en/src/org/lunifera/metamodel/dsl/organization/en/validation/OrganizationDslJavaValidator.java
@@ -0,0 +1,13 @@
+package org.lunifera.metamodel.dsl.organization.en.validation;
+ 
+
+public class OrganizationDslJavaValidator extends AbstractOrganizationDslJavaValidator {
+
+//	@Check
+//	public void checkGreetingStartsWithCapital(Greeting greeting) {
+//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
+//			warning("Name should start with a capital", MyDslPackage.Literals.GREETING__NAME);
+//		}
+//	}
+
+}
