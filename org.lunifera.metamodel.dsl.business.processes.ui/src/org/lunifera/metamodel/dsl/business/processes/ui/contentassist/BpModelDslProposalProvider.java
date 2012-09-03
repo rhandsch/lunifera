5b01fadbaadd7dc54395101c01e03680ea75aefd
Cristiano Gaviao <cvgaviao@gmail.com>
2012-09-03 13:48:36 -0300

    modified wizards of bpmodel project.modified wizards of bpmodel project.

--- org.lunifera.metamodel.dsl.business.processes.ui/src/org/lunifera/metamodel/dsl/business/processes/ui/contentassist/BpModelDslProposalProvider.java
+++ org.lunifera.metamodel.dsl.business.processes.ui/src/org/lunifera/metamodel/dsl/business/processes/ui/contentassist/BpModelDslProposalProvider.java
@@ -13,10 +13,61 @@
 */
 package org.lunifera.metamodel.dsl.business.processes.ui.contentassist;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.xtext.Assignment;
+import org.eclipse.xtext.GrammarUtil;
+import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.ui.editor.contentassist.ConfigurableCompletionProposal;
+import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
+import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
+import org.eclipse.xtext.ui.editor.contentassist.PrefixMatcher;
+import org.eclipse.xtext.util.Strings;
 import org.lunifera.metamodel.dsl.business.processes.ui.contentassist.AbstractBpModelDslProposalProvider;
 /**
  * see http://www.eclipse.org/Xtext/documentation/latest/xtext.html#contentAssist on how to customize content assistant
  */
 public class BpModelDslProposalProvider extends AbstractBpModelDslProposalProvider {
 
+	public void complete_Fqn(EObject model, RuleCall ruleCall, final ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		if (doCreateIdProposals()) {
+			PrefixMatcher newMatcher = new PrefixMatcher() {
+				@Override
+				public boolean isCandidateMatchingPrefix(String name, String prefix) {
+					String strippedName = name;
+					if (name.startsWith("^") && !prefix.startsWith("^")) {
+						strippedName = name.substring(1);
+					}
+					return context.getMatcher().isCandidateMatchingPrefix(strippedName, prefix);
+				}
+			};
+			ContentAssistContext myContext = context.copy().setMatcher(newMatcher).toContext();
+			String feature = getAssignedFeature(ruleCall);
+			String proposalText = feature != null ? feature : Strings.toFirstUpper(ruleCall.getRule().getName().toLowerCase());
+			String displayText = proposalText;
+			if (feature != null)
+				displayText = proposalText + " - " + ruleCall.getRule().getName();
+			proposalText = getValueConverter().toString(proposalText, ruleCall.getRule().getName());
+			ICompletionProposal proposal = createCompletionProposal(proposalText, displayText, null, myContext);
+			if (proposal instanceof ConfigurableCompletionProposal) {
+				ConfigurableCompletionProposal configurable = (ConfigurableCompletionProposal) proposal;
+				configurable.setSelectionStart(configurable.getReplacementOffset());
+				configurable.setSelectionLength(proposalText.length());
+				configurable.setAutoInsertable(false);
+				configurable.setSimpleLinkedMode(myContext.getViewer(), '\t', ' ');
+			}
+			acceptor.accept(proposal);
+		}
+	}
+	private String getAssignedFeature(RuleCall call) {
+		Assignment ass = GrammarUtil.containingAssignment(call);
+		if (ass != null) {
+			String result = ass.getFeature();
+			if (result.equals(result.toLowerCase()))
+				result = Strings.toFirstUpper(result);
+			return result;
+		}
+		return null;
+	}
 }
--- org.lunifera.metamodel.dsl.business.processes.ui/src/org/lunifera/metamodel/dsl/business/processes/ui/wizards/LuniferaNewBPModelWizard.java
+++ org.lunifera.metamodel.dsl.business.processes.ui/src/org/lunifera/metamodel/dsl/business/processes/ui/wizards/LuniferaNewBPModelWizard.java
@@ -10,151 +10,62 @@
  *******************************************************************************/
 package org.lunifera.metamodel.dsl.business.processes.ui.wizards;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.core.runtime.*;
-import org.eclipse.jface.operation.*;
-import java.lang.reflect.InvocationTargetException;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.core.resources.*;
-import org.eclipse.core.runtime.CoreException;
-import java.io.*;
-import org.eclipse.ui.*;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.internal.ide.DialogUtil;
+import org.eclipse.ui.internal.wizards.newresource.ResourceMessages;
+import org.eclipse.ui.wizards.newresource.BasicNewFileResourceWizard;
 
-/**
- * This is a sample new wizard. Its role is to create a new file 
- * resource in the provided container. If the container resource
- * (a folder or a project) is selected in the workspace 
- * when the wizard is opened, it will accept it as the target
- * container. The wizard creates one file with the extension
- * "organization". If a sample multi-page editor (also available
- * as a template) is registered for the same extension, it will
- * be able to open it.
- */
 
-public class LuniferaNewBPModelWizard extends Wizard implements INewWizard {
-	private LuniferaNewBPModelWizardPage page;
-	private ISelection selection;
+@SuppressWarnings("restriction")
+public class LuniferaNewBPModelWizard extends BasicNewFileResourceWizard {
+	private LuniferaNewBPModelWizardPage mainPage;
 
-	/**
-	 * Constructor for LuniferaNewBPModelWizard.
-	 */
 	public LuniferaNewBPModelWizard() {
 		super();
-		setNeedsProgressMonitor(true);
 	}
-	
-	/**
-	 * Adding the page to the wizard.
-	 */
 
 	public void addPages() {
-		page = new LuniferaNewBPModelWizardPage(selection);
-		addPage(page);
+		mainPage = new LuniferaNewBPModelWizardPage(getSelection());
+		addPage(mainPage);
 	}
 
-	/**
-	 * This method is called when 'Finish' button is pressed in
-	 * the wizard. We will create an operation and run it
-	 * using wizard as execution context.
-	 */
 	public boolean performFinish() {
-		final String containerName = page.getContainerName();
-		final String fileName = page.getFileName();
-		IRunnableWithProgress op = new IRunnableWithProgress() {
-			public void run(IProgressMonitor monitor) throws InvocationTargetException {
-				try {
-					doFinish(containerName, fileName, monitor);
-				} catch (CoreException e) {
-					throw new InvocationTargetException(e);
-				} finally {
-					monitor.done();
-				}
-			}
-		};
-		try {
-			getContainer().run(true, false, op);
-		} catch (InterruptedException e) {
-			return false;
-		} catch (InvocationTargetException e) {
-			Throwable realException = e.getTargetException();
-			MessageDialog.openError(getShell(), "Error", realException.getMessage());
+		IFile file = mainPage.createNewFile();
+		if (file == null) {
 			return false;
 		}
+
+		selectAndReveal(file);
+
+		// Open editor on new file.
+		IWorkbenchWindow dw = getWorkbench().getActiveWorkbenchWindow();
+		try {
+			if (dw != null) {
+				IWorkbenchPage page = dw.getActivePage();
+				if (page != null) {
+					IDE.openEditor(page, file, true);
+				}
+			}
+		} catch (PartInitException e) {
+			DialogUtil.openError(dw.getShell(),
+					ResourceMessages.FileResource_errorMessage, e.getMessage(),
+					e);
+		}
+
 		return true;
 	}
-	
-	/**
-	 * The worker method. It will find the container, create the
-	 * file if missing or just replace its contents, and open
-	 * the editor on the newly created file.
-	 */
 
-	private void doFinish(
-		String containerName,
-		String fileName,
-		IProgressMonitor monitor)
-		throws CoreException {
-		// create a sample file
-		monitor.beginTask("Creating " + fileName, 2);
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IResource resource = root.findMember(new Path(containerName));
-		if (!resource.exists() || !(resource instanceof IContainer)) {
-			throwCoreException("Container \"" + containerName + "\" does not exist.");
-		}
-		IContainer container = (IContainer) resource;
-		final IFile file = container.getFile(new Path(fileName));
-		try {
-			InputStream stream = openContentStream();
-			if (file.exists()) {
-				file.setContents(stream, true, true, monitor);
-			} else {
-				file.create(stream, true, monitor);
-			}
-			stream.close();
-		} catch (IOException e) {
-		}
-		monitor.worked(1);
-		monitor.setTaskName("Opening file for editing...");
-		getShell().getDisplay().asyncExec(new Runnable() {
-			public void run() {
-				IWorkbenchPage page =
-					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-				try {
-					IDE.openEditor(page, file, true);
-				} catch (PartInitException e) {
-				}
-			}
-		});
-		monitor.worked(1);
-	}
-	
-	/**
-	 * We will initialize file contents with a sample text.
-	 */
-
-	private InputStream openContentStream() {
-		String contents =
-			"This is the initial file contents for *.organization file that should be word-sorted in the Preview page of the multi-page editor";
-		return new ByteArrayInputStream(contents.getBytes());
+	public void init(IWorkbench workbench, IStructuredSelection currentSelection) {
+		super.init(workbench, currentSelection);
 	}
 
-	private void throwCoreException(String message) throws CoreException {
-		IStatus status =
-			new Status(IStatus.ERROR, "org.lunifera.eclipse.ui", IStatus.OK, message, null);
-		throw new CoreException(status);
-	}
-
-	/**
-	 * We will accept the selection in the workbench to see if
-	 * we can initialize from it.
-	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
-	 */
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		this.selection = selection;
+	protected void initializeDefaultPageImageDescriptor() {
+		super.initializeDefaultPageImageDescriptor();
 	}
 }
